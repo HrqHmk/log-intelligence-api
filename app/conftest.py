3a8394b4a37ff87eccb74bc3d5bb0847a4bb5276
Henrique Makita <henrique_hmk@yahoo.com>
2026-03-22 09:17:57 -0300

    update: update conftest to seed database

--- app/conftest.py
+++ app/conftest.py
@@ -1,12 +1,34 @@
+# pylint:disable = W1514
 import os
+from pathlib import Path
 import pytest
+import pytest_asyncio
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import text
 
 load_dotenv()
 
 TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL")
+
+@pytest_asyncio.fixture(autouse=True)
+async def setup_and_teardown_db(session_factory):
+    sql_path = Path("init/seed_log.sql")
+
+    async with session_factory() as session:
+        await session.execute(text("TRUNCATE TABLE request_logs RESTART IDENTITY CASCADE"))
+
+        with open(sql_path, "r") as f:
+            sql_script = f.read()
+
+        await session.execute(text(sql_script))
+        await session.commit()
+
+    yield
+    async with session_factory() as session:
+        await session.execute(text("TRUNCATE TABLE request_logs RESTART IDENTITY CASCADE"))
+        await session.commit()
 
 @pytest.fixture(name="engine")
 def engine_fixture():
@@ -20,7 +42,7 @@ def session_factory_fixture(engine):
         expire_on_commit=False
     )
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def db_session(session_factory):
     async with session_factory() as session:
         trans = await session.begin()
