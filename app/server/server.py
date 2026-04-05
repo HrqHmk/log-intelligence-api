eecf8954b837bba2ed4ec6c38ee012860fca0a22
HrqHmk <henrique_hmk@yahoo.com>
2026-04-05 10:45:41 -0300

    Delete app/server directory

--- app/server/server.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from app.main.routes.request_log_routes import request_log_routes
-
-app = FastAPI()
-app.include_router(request_log_routes)
-@app.get("/")
-async def root():
-    return {"message": "API is running!"}
